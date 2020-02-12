f8eea8d86b9ecdceabfed9563c4396c8cb120f42
wlckdrms94 <wlckdrms94@naver.com>
2020-02-13 01:30:15 +0900

    백준
    백준_03

--- Main.java
+++ Main.java
@@ -4,18 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        while (sc.hasNextInt()) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            if(A > 0){
-                if(B < 10){
-                    int C = A + B;
-                    System.out.println(C);
-                }
+        int originNumber = sc.nextInt(); // 최초값 받아오기 ex ) 26
+        int left = originNumber / 10; // 2 저장
+        int right = originNumber % 10; // 6 저장
+
+        int cycleResult = 0; // 사이클 돈 횟수 구하기
+        int A = originNumber / 10; // A 에 저장된 값에서 10을 나눠서 B에 저장 앞쪽값은 한번만 구하면 된다
+        int B = originNumber % 10; // 저장
+        int C,D;
+        while (true) {
+            cycleResult++; //사이클 1회 추가
+            
+            C = A + B ; // A+B
+            D = C % 10; // C 의 끝자리 구하기
+            A = B;
+            B = D;
+            
+            if(A == left && B == right){ // A 와 B 가 원래 숫자와 완벽히 일치할때까지 구해라 이놈아
+                System.out.print(cycleResult);
+                break;
             }
         }
-        
         sc.close();
     }
 }

5e58b2abdcf39f1fe0c398e369f6171ac6876adc
wlckdrms94 <wlckdrms94@naver.com>
2020-02-25 00:02:23 +0900

    백준_05
    일차원 배열

--- Main.java
+++ Main.java
@@ -1,30 +1,24 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int originNumber = sc.nextInt(); // 최초값 받아오기 ex ) 26
-        int left = originNumber / 10; // 2 저장
-        int right = originNumber % 10; // 6 저장
+        Scanner sc = new Scanner(System.in); //숫자를 받겠다 
+        int writeNumber = sc.nextInt(); // 처음 받은 숫자를 저장
 
-        int cycleResult = 0; // 사이클 돈 횟수 구하기
-        int A = originNumber / 10; // A 에 저장된 값에서 10을 나눠서 B에 저장 앞쪽값은 한번만 구하면 된다
-        int B = originNumber % 10; // 저장
-        int C,D;
-        while (true) {
-            cycleResult++; //사이클 1회 추가
-            
-            C = A + B ; // A+B
-            D = C % 10; // C 의 끝자리 구하기
-            A = B;
-            B = D;
-            
-            if(A == left && B == right){ // A 와 B 가 원래 숫자와 완벽히 일치할때까지 구해라 이놈아
-                System.out.print(cycleResult);
-                break;
-            }
+        int[] area = new int[writeNumber]; // 처음받은 숫자만큼 배열 공간 생성
+
+        for(int i = 0; i < writeNumber; i++){
+            area[i] = sc.nextInt(); // 처음 받은 숫자만큼 배열 번호에 맞게 저장
         }
+
+        Arrays.sort(area);
+        
+        System.out.println(area[0]);
+        System.out.println(area[area.length - 1]);
+
         sc.close();
+
     }
 }
